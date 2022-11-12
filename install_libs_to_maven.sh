7c44eb99c9549630ab4e93c0ac981a7afc33db85
StasyanOi <iluvstrwrs@mail.ru>
2022-11-12 19:58:39 +0200

    added libraries to copy to maven

--- install_libs_to_maven.sh
+++ install_libs_to_maven.sh
@@ -3,5 +3,5 @@ LIBS_TO_INSTALL=$(ls ./lib)
 for library in $LIBS_TO_INSTALL ; do
     library=${library%".jar"}
     echo $library
-    mvn install:install-file -Dfile=./mpj-V0_44/lib/$library.jar -DgroupId=mpi.local -DartifactId=$library -Dversion=LOCAL -Dpackaging=jar
+    mvn install:install-file -Dfile=./lib/$library.jar -DgroupId=mpi.local -DartifactId=$library -Dversion=LOCAL -Dpackaging=jar
 done

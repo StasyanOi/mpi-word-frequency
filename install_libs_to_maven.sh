45f0b6137e7d6533afa132070087f2348706a4c0
StasyanOi <iluvstrwrs@mail.ru>
2023-01-22 20:10:04 +0200

    fixed "install libraries to maven" script

--- install_libs_to_maven.sh
+++ install_libs_to_maven.sh
@@ -2,8 +2,6 @@ LIBS_TO_INSTALL=$(ls ./lib)
 
 for library in $LIBS_TO_INSTALL ; do
   library=${library%".jar"}
-  if [find $HOME/.m2/repository -type f -name $library]; then
-    echo $library
-    mvn install:install-file -Dfile=./lib/$library.jar -DgroupId=mpi.local -DartifactId=$library -Dversion=LOCAL -Dpackaging=jar
-  fi
+  echo $library
+  mvn install:install-file -Dfile=./lib/$library.jar -DgroupId=mpi.local -DartifactId=$library -Dversion=LOCAL -Dpackaging=jar
 done
